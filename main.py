540665015072729ec8d12361092a8b0e9194c115
rafaelreis8000 <priv.rafa.reis@gmail.com>
2025-04-23 17:00:55 -0300

    início do desenvolvimento da tela inicial

---
+++ main.py
@@ -0,0 +1,10 @@
+while True:
+
+    tela_inicial = input(
+        '{}\n       JOKENPO!'
+        '\n\n     [ 1 ] JOGAR'
+        '\n     [ 2 ] REGRAS'
+        '\n     [ 3 ] SAIR\n{}'
+        '\n --> '
+        .format('='*22 , '='*22)
+    )
